e754c5deb1b295c2a4822aa2e55661fb0f8efd3b
BlueGlassBlock <blueglassblock@outlook.com>
2022-11-22 09:32:21 +0800

    feat: add basic text parsers

---
+++ src/graiax/shortcut/_typing_util.py
@@ -0,0 +1,47 @@
+import contextlib
+import sys
+import types
+from typing import Tuple, Type, TypeVar, Union
+
+from typing_extensions import Annotated, Any, TypeGuard, get_args, get_origin
+
+_T = TypeVar("_T")
+
+
+def is_subclass(obj: Any, cls: Type[_T]) -> TypeGuard[Type[_T]]:
+    return isinstance(obj, type) and issubclass(obj, cls)
+
+
+AnnotatedType = type(Annotated[int, lambda x: x > 0])
+Unions = (Union, types.UnionType) if sys.version_info >= (3, 10) else (Union,)
+
+
+def generic_issubclass(cls: Any, par: Union[type, Any, Tuple[type, ...]]) -> bool:
+    """检查 cls 是否是 args 中的一个子类, 支持泛型, Any, Union
+
+    Args:
+        cls (type): 要检查的类
+        par (Union[type, Any, Tuple[type, ...]]): 要检查的类的父类
+
+    Returns:
+        bool: 是否是父类
+    """
+    if par is Any:
+        return True
+    with contextlib.suppress(TypeError):
+        if isinstance(par, AnnotatedType):
+            return generic_issubclass(cls, get_args(par)[0])
+        if isinstance(par, (type, tuple)):
+            return issubclass(cls, par)
+        if get_origin(par) in Unions:
+            return any(generic_issubclass(cls, p) for p in get_args(par))
+        if isinstance(par, TypeVar):
+            if par.__constraints__:
+                return any(generic_issubclass(cls, p) for p in par.__constraints__)
+            if par.__bound__:
+                return generic_issubclass(cls, par.__bound__)
+    return False
+
+
+def is_union(obj: Any) -> bool:
+    return get_origin(obj) in Unions
--- src/graiax/shortcut/_util.py
+++ src/graiax/shortcut/_util.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+import re
+
 from graia.amnesia.message import Element, MessageChain, Text
 
 
@@ -11,3 +15,32 @@ def text(string: str) -> Text:
     from graia.amnesia import message
 
     return message.__text_element_class__(string)
+
+
+def map_chain(chain: MessageChain) -> tuple[str, dict[str, Element]]:
+    elem_mapping: dict[str, Element] = {}
+    elem_str_list: list[str] = []
+    for i, elem in enumerate(chain.content):
+        if not isinstance(elem, Text):
+            elem_mapping[str(i)] = elem
+            elem_str_list.append(f"\x02{i}_{elem.__class__.__name__}\x03")
+        else:
+            elem_str_list.append(elem.text)
+    return "".join(elem_str_list), elem_mapping
+
+
+__element_pattern = re.compile("(\x02\\w+\x03)")
+
+
+def unmap_chain(string: str, mapping: dict[str, Element]) -> MessageChain:
+    elements: list[Element] = []
+    for x in __element_pattern.split(string):
+        if x:
+            if x[0] == "\x02" and x[-1] == "\x03":
+                index, class_name = x[1:-1].split("_")
+                if mapping[index].__class__.__name__ != class_name:
+                    raise ValueError("Validation failed: not matching element type!")
+                elements.append(mapping[index])
+            else:
+                elements.append(text(x))
+    return chain(elements)
